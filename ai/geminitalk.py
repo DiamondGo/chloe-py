14f9faf09ce92f086240dfe63e1a79c8ae7bb40d
Ken <kenxieca@gmail.com>
2024-11-09 20:46:25 -0800

    use uploaded file to gemini instead of embeded file

--- ai/geminitalk.py
+++ ai/geminitalk.py
@@ -1,13 +1,34 @@
 from datetime import datetime, timedelta
 from typing import List
+import time
 
 import google.generativeai as genai
+from google.generativeai import caching
 import PIL.Image
 
 from common import GeminiConfig
 from definition import Talk
 
 from loguru import logger
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    }
+]
 
 configuredKey = None
 def configureGemini(apiKey: str):
@@ -16,41 +37,17 @@ def configureGemini(apiKey: str):
         configuredKey = apiKey
         genai.configure(api_key=configuredKey)
         
-
 class GeminiTalk(Talk):
 
     def __init__(self, botName: str, config: GeminiConfig) -> None:
         self.apiKey: str = config.apiKey
         configureGemini(self.apiKey)
         
-        safety_settings = [
-            {
-                "category": "HARM_CATEGORY_HARASSMENT",
-                "threshold": "BLOCK_NONE"
-            },
-            {
-                "category": "HARM_CATEGORY_HATE_SPEECH",
-                "threshold": "BLOCK_NONE"
-            },
-            {
-                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_NONE"
-            },
-            {
-                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_NONE"
-            }
-        ]
-
-        self.model = genai.GenerativeModel(
-            model_name=config.model,
-            safety_settings=safety_settings
-        )
+        self.initModel(config.model)
         self.lastMessage = datetime.now()
         self.ctxTimeout = timedelta(seconds=config.contextTimeout)
         self.visionModel = genai.GenerativeModel(config.visionModel)
         self.pendingContent = []
-        self.hasImage = False
         self.greeting = [
             {
                 'role': 'user',
@@ -62,46 +59,41 @@ class GeminiTalk(Talk):
             },
         ]
         self.pendingContent.extend(self.greeting)
+    
+    def initModel(self, model):
+        self.model = genai.GenerativeModel(
+            model_name=model,
+            safety_settings=safety_settings
+        )
+        
+    def expireContext(self):
+        self.pendingContent = []
+        self.pendingContent.extend(self.greeting)
 
     def ask(self, q: str) -> str:
         now = datetime.now()
         if now > self.lastMessage + self.ctxTimeout:
-            self.pendingContent = []
-            self.pendingContent.extend(self.greeting)
-            self.hasImage = False
+            self.expireContext()
         
-        if len(self.pendingContent) > 2 and self.pendingContent[-1]['role'] == 'user':
-            self.pendingContent[-1]['parts'].append(q)
-        else:
-            self.pendingContent.append(
-                {
-                    'role': 'user',
-                    'parts': [q]
-                }
-            )
+        self.pendingContent.append(
+            {
+                'role': 'user',
+                'parts': [q]
+            }
+        )
             
         retry = 3
         while retry > 0:
             try:
-                if self.hasImage:
-                    resp = self.visionModel.generate_content(self.pendingContent[-1]) # gemini-pro-vision is not enabled for multi-turn conversation
-                    resp.resolve()
+                resp = self.model.generate_content(self.pendingContent)
+                resp.resolve()
 
-                    self.pendingContent = []
-                    self.pendingContent.extend(self.greeting)
-                    self.hasImage = False
-                    
-                    text = resp.text
-                else:
-                    resp = self.model.generate_content(self.pendingContent)
-                    resp.resolve()
+                self.pendingContent.append({
+                    'role': 'model',
+                    'parts': [resp.text]
+                })
 
-                    self.pendingContent.append({
-                        'role': 'model',
-                        'parts': [resp.text]
-                    })
-
-                    text = resp.text
+                text = resp.text
                 
                 break
             except Exception as e:
@@ -114,21 +106,27 @@ class GeminiTalk(Talk):
     def prepareImages(self, images: List[str]=None) -> None:
         now = datetime.now()
         if now > self.lastMessage + self.ctxTimeout:
-            self.pendingContent = []
-            self.pendingContent.extend(self.greeting)
-            self.hasImage = False
+            self.expireContext()
         
-        parts = []
-        for f in images:
-            img = PIL.Image.open(f)
-            parts.append(img)
-            self.hasImage = True
-        
-        self.pendingContent.append(
-            {
-                'role': 'user',
-                'parts': parts
-            }
-        )
+        parts = self.uploadFiles(images)
+        if len(parts) > 0:
+            self.pendingContent.append(
+                {
+                    'role': 'user',
+                    'parts': parts
+                }
+            )
 
         self.lastMessage = datetime.now()
+
+
+    def uploadFiles(self, files: List[str]=None):
+        parts = []
+        for f in files:
+            upload = genai.upload_file(path=f)
+            while upload.state.name == 'PROCESSING':
+                time.sleep(2)
+                upload = genai.get_file(upload.name)
+            parts.append(upload)
+        return parts
+ 
